2da28e5525db2f20c31ac085ad113b3150cdbc7a
Ka Fai Lo <ka_fai_lo@manulife.com>
2020-01-10 11:28:57 +0000

    Test

---
+++ server.js
@@ -0,0 +1,20 @@
+console.log("Server starting");
+
+
+var express = require('express'),
+  app = express(),
+  port = 3000;
+
+app.use(express.static('public'));
+
+var routes = require("./api/routes");
+routes(app);
+
+if (! module.parent) {
+  app.listen(port);
+}
+
+module.exports = app
+
+console.log("Server running on port " + port);
+ 
